ca3601f1ba4e7b4c9e2520acecf9d7ba79ba1c31
Guillaume VINDRY <guillaume.v.gv@gmail.com>
2024-07-04 02:20:11 +0200

    intrinsic rotation

--- 3D/cube_animate.py
+++ 3D/cube_animate.py
@@ -14,13 +14,14 @@ import matplotlib.animation as animation
 #             Point3D(np.array([2,2,1,1])),
 #             ])
 from polyhedra import Cube
-c = Cube(Point3D(np.array([-1,1,3,1])),Point3D(np.array([2,2,4,1])))
+c = Cube(Point3D(np.array([-1.,1,3,1])),Point3D(np.array([2.,2,4,1])))
 print(c.sides)
 proj = Projection()
 xmin,xmax = -2,2
 ymin,ymax =  -2,2
 from  scipy.spatial.transform import Rotation as R
 
+intrinsic_rotc = R.from_quat([ 1*np.sin(np.pi/100),0.5*np.sin(np.pi/100),0, np.cos(np.pi/100)])
 fig = plt.figure()
 def update(num, proj):
     theta = np.pi*num/30
@@ -28,8 +29,7 @@ def update(num, proj):
     proj= Projection(translation=num*1e-1*np.array([0.1,0.1,0.1]),rotation=rot)
     plt.clf()
     ls= []
-
-    
+    c.rotate(intrinsic_rotc)
     for s in c.sides:
         ls.append(plt.plot(*s.get_plottable(proj),color="r"))
         ax = plt.gca()
--- 3D/polyhedra.py
+++ 3D/polyhedra.py
@@ -1,7 +1,10 @@
 from geometry import Point2D,Point3D,Projection,Polygon
+from  scipy.spatial.transform import Rotation
 
 class Cube:
     def __init__(self, p1:Point3D, p2:Point3D):
+        self.p1 = p1
+        self.p2 = p2
         p1x = Point3D(p1.pos)
         p1x.pos[0] = p2.pos[0]
         p1y = Point3D(p1.pos)
@@ -16,10 +19,15 @@ class Cube:
         p1zx = Point3D(p2.pos)
         p1zx.pos[1] = p1.pos[1]
 
-        lp1 = [p1x,p1y,p1z]
-        lp2 = [p1yz,p1zx,p1xy]
-        print(f"{lp1=}{lp2=}")
-        
+        self.lp1 = [p1x,p1y,p1z]
+        self.lp2 = [p1yz,p1zx,p1xy]
+
+        self._make_sides()
+
+    def _make_sides(self):
+        p1,p2 = self.p1,self.p2
+        lp1,lp2 = self.lp1,self.lp2
+
         self.sides = []
         for i in range(3):
             j,k = (i+1)%3,(i+2)%3
@@ -27,3 +35,17 @@ class Cube:
             self.sides.append(side1)
             side2 = Polygon([p2,lp2[j],lp1[i],lp2[k]])
             self.sides.append(side2)
+
+    def rotate(self,r:Rotation):
+        baryc = 0.5*(self.p1.pos[:3] + self.p2.pos[:3])
+        self.p1.pos[:3] = baryc+ r.apply(self.p1.pos[:3]-baryc)
+        self.p2.pos[:3] = baryc + r.apply(self.p2.pos[:3]-baryc)
+
+        for pt in self.lp1:
+            pt.pos[:3] = baryc+ r.apply(pt.pos[:3]-baryc)
+        for pt in self.lp2:
+            pt.pos[:3] = baryc+ r.apply(pt.pos[:3]-baryc)
+
+        self._make_sides()
+
+
